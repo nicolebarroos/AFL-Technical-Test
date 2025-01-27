878e8c427d77d06eef0d6ab337dff2193c1cd2d8
Nicole Barros <magalu@WNLEC-BSLR6Q3.magazineluiza.intranet>
2025-01-27 14:20:27 -0300

    chore: initial project setup with Poetry, Docker, and database

---
+++ database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Database:
+    _instance = None
+    _engine = None
+    _SessionLocal = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Database, cls).__new__(cls)
+            cls._setup()
+        return cls._instance
+
+    @classmethod
+    def _setup(cls):
+        DATABASE_URL = os.getenv("DATABASE_URL")
+        cls._engine = create_engine(DATABASE_URL)
+        cls._SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=cls._engine)
+
+    def get_session(self):
+        return self._SessionLocal()
+
+Base = declarative_base()
+db_instance = Database()
